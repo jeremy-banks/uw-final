19c9abcac74e00fc86c12b5aa672e4d81b36de27
Jeremy B <workjeremyb@gmail.com>
2024-08-29 13:48:46 -0700

    scene changes but i forgot to add singleton logic to the player lol

--- Assets/Scenes/SceneSwapPad.cs
+++ Assets/Scenes/SceneSwapPad.cs
@@ -1,20 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SceneSwapPad : MonoBehaviour
 {
     public int SceneToSwapTo;
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter(Collider other)
     {
-        
+        // Check if the object colliding with the pad is tagged as "Player"
+        if (other.CompareTag("Player"))
+        {
+            // Load the scene with the index specified in the public variable
+            SceneManager.LoadScene(SceneToSwapTo);
+        }
     }
 }
