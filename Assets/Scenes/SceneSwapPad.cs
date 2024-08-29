3c8e14878eb78326d5f6177ac85a7b1905d60d42
Jeremy B <workjeremyb@gmail.com>
2024-08-29 13:41:21 -0700

    initial work for scene swap pad

---
+++ Assets/Scenes/SceneSwapPad.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneSwapPad : MonoBehaviour
+{
+    public int SceneToSwapTo;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
