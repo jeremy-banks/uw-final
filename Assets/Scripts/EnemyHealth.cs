bc634b50b78c41fb18876ee471ad758fa0933a66
Jeremy B <workjeremyb@gmail.com>
2024-08-29 18:06:29 -0700

    prelim enemy health script

--- Assets/Scripts/EnemyHealth.cs
+++ Assets/Scripts/EnemyHealth.cs
@@ -4,18 +4,43 @@ using UnityEngine;
 
 public class EnemyHealth : MonoBehaviour
 {
-    public bool isEnemyAlive;
-    public int health;
+    public int EnemyID;
+    int healthCurrent;
+    public int healthStarting;
 
     // Start is called before the first frame update
     void Start()
     {
-        // check if enemy is alive, if so give it hp
+        // check if enemy is dead
+        if (EnemyState.Instance.IsEnemyDead(EnemyID))
+        {
+            // enemy is dead so deactivate
+            gameObject.SetActive(false);
+            return;
+        }
+        else
+        {
+            // enemy is not dead so give him health
+            healthCurrent = healthStarting;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        //if HP<0 then kill enemy
+        //if hp less than 1 then die
+        if (healthCurrent < 1) Die();
+    }
+
+    void Die()
+    {
+        EnemyState.Instance.MarkEnemyDead(EnemyID);
+        gameObject.SetActive(false);
+        return;
+    }
+
+    public void DealDamage()
+    {
+        healthCurrent -= 1;
     }
 }
