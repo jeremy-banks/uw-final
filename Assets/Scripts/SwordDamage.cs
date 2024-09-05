37c3155dab1d5e7cc3f78bb1fc973c2899c9b9e5
Jeremy B <workjeremyb@gmail.com>
2024-09-04 20:55:47 -0700

    changing sworddamage from OnTriggerEnter to OnCollisionEnter

--- Assets/Scripts/SwordDamage.cs
+++ Assets/Scripts/SwordDamage.cs
@@ -4,38 +4,39 @@ using System.Collections;
 public class SwordDamage : MonoBehaviour
 {
     Rigidbody rb;
-    void OnTriggerEnter(Collider other)
-    {
-        //Debug.Log("Trigger detected with: " + other.gameObject.name);
 
-        // Check if the object that entered the trigger has a method named "Damage"
-        IDamageable damageable = other.gameObject.GetComponent<IDamageable>();
+    void OnCollisionEnter(Collision collision)
+    {
+        //Debug.Log("Collision detected with: " + collision.gameObject.name);
+
+        // Check if the object that collided has a method named "Damage"
+        IDamageable damageable = collision.gameObject.GetComponent<IDamageable>();
 
         if (damageable != null)
         {
-            //Debug.Log("Damageable object found: " + other.gameObject.name);
+            //Debug.Log("Damageable object found: " + collision.gameObject.name);
 
             // Call the Damage method on the damageable object
             damageable.Damage();
 
             // Also add a little directional shove for effect
-            rb = other.GetComponent<Rigidbody>();
+            rb = collision.rigidbody;
             if (rb != null)
             {
                 Debug.Log("rb is not null");
                 // Calculate direction to push the object away from the player
-                Vector3 pushDirection = (other.transform.position - transform.position).normalized;
+                Vector3 pushDirection = (collision.transform.position - transform.position).normalized;
 
                 // Apply that force
                 rb.AddForce(pushDirection * 300f, ForceMode.Impulse);
 
-                // Stop the force (otherwise object just floats away
+                // Stop the force (otherwise object just floats away)
                 StartCoroutine(StopVelocityCoroutine());
             }
         }
         else
         {
-            Debug.Log("No IDamageable component found on: " + other.gameObject.name);
+            Debug.Log("No IDamageable component found on: " + collision.gameObject.name);
         }
     }
 
