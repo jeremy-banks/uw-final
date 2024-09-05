5511b212c93874a8a6209d057b2e5458d10930ff
Jeremy B <workjeremyb@gmail.com>
2024-09-05 07:49:37 -0700

    adding prelim work for archer and fireguy enemies

---
+++ Assets/Scripts/ArcherSentry.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArcherSentry : MonoBehaviour
+{
+
+    // Acquire player position
+    // Shoot projectile at player using force impluse
+    // Damage player on hit
+}
